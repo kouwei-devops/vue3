097257832483525ef2fc2384669ae4ff7b3bd7a8
寇伟 <1461785165@qq.com>
2026-02-01 16:41:59 +0800

    41

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -25,6 +25,7 @@
   </el-card>
 </div>
 <div style=" margin: auto; width: 70%; margin-bottom: 20px; text-align: center; font-size: 28px; font-weight: 600;
+margin-bottom: 20px; border-radius: 12px;
 box-shadow: 0 4px 12px rgba(0, 0, 0, 0.12);
 background: #fff;
 ">
